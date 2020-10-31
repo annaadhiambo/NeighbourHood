38d970e4153eeea665629b1775fe528a0e974e58
annaadhiambo <annaadhiambo01@gmail.com>
2020-10-31 04:38:50 +0300

    added class notification form

--- hood/forms.py
+++ hood/forms.py
@@ -1,2 +1,9 @@
 from django import forms
 from .models import notifications, Business, Profile, Comment
+
+
+
+class notificationsForm(forms.ModelForm):
+    class Meta:
+        model = notifications
+        exclude = ['author', 'neighbourhood', 'post_date']

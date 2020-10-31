e42b16e48816d3ccafb174d2cf702b6e1df90472
annaadhiambo <annaadhiambo01@gmail.com>
2020-10-31 04:34:38 +0300

    imported admins

--- hood/admin.py
+++ hood/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from .models import Neighbourhood, healthservices, notifications, Business, Health, Authorities, Profile
+
 
 # Register your models here.
 admin.site.register(Neighbourhood)

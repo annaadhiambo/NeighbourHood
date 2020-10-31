5a26f573c9a6d59d7d866fdd6d5f3a1ec9ea824c
annaadhiambo <annaadhiambo01@gmail.com>
2020-10-31 04:39:53 +0300

    added class businessform

--- hood/forms.py
+++ hood/forms.py
@@ -7,3 +7,14 @@ class notificationsForm(forms.ModelForm):
     class Meta:
         model = notifications
         exclude = ['author', 'neighbourhood', 'post_date']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['username']
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ['owner', 'neighbourhood']
+

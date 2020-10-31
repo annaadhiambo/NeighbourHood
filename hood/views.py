3d98ccefee49510c9ca874712971410349283710
annaadhiambo <annaadhiambo01@gmail.com>
2020-10-31 04:17:21 +0300

    defined index

--- hood/views.py
+++ hood/views.py
@@ -8,3 +8,15 @@ import datetime as datetime
 import json
 from django.db.models import Q
 from django.contrib.auth.models import User
+
+# Create your views here.
+def index(request):
+    try:
+        if not request.user.is_authenticated:
+            return redirect('/accounts/login/')
+        current_user = request.user
+        profile = Profile.objects.get(username=current_user)
+    except ObjectDoesNotExist:
+        return redirect('create-profile')
+
+    return render(request, 'index.html')

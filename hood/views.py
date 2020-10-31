39534baf58b694060e7b4348746517311ec247bd
annaadhiambo <annaadhiambo01@gmail.com>
2020-10-31 04:16:34 +0300

    imported views

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Neighbourhood, healthservices, Business, Health, Authorities, Profile, notifications, Comment
+from .forms import notificationsForm, ProfileForm, BusinessForm, CommentForm
+import datetime as datetime
+import json
+from django.db.models import Q
+from django.contrib.auth.models import User
